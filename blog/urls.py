568f61829a2886d0e6c39ba24bd10dae3464cc76
Jason <jlavery828@gmail.com>
2019-10-15 17:00:43 -0400

    email-list-signup

--- blog/urls.py
+++ blog/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('', index),
     path('blog/', blog, name='post-list'),
     path('search/', search, name='search'),
-    path('email-signup/', email_list_signup, name='email_list_signup'),
+    path('email-signup/', email_list_signup, name='email-list-signup'),
     path('create/', post_create, name='post-create'),
     path('post/<id>/', post, name='post-detail'),
     path('post/<id>/update/', post_update, name='post-update'),
